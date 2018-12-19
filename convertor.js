02cfa28508dbbb1e1ddd624d21c2cc2d9b1c7622
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-19 22:27:53 +0200

    get number of digits from input number

--- convertor.js
+++ convertor.js
@@ -1,26 +1,10 @@
-window.doConvert = function () {
-  // turn the input number into the textual English language equivalent
-  // e.g. 104 becomes one hundred and four
-  // e.g. 12345 becomes twelve thousand three hundred and forty five
-  var numberInput = document.getElementById("numberInput").value;
-  var outputText = "";
-
-// Create a hardcoded map for n < 20 (all the values when turned to string are uniques)
-/*
-
-Create a hardcoded map for concatenations for n > 20 && n < 100: 
-i.e. 
-tw[o] + en + "ty" 
-th[ree] + ir + "ty"
-fo[ur] + "r" + "ty"
-fi[ve] "f" + "ty" 
-sixty = six + "ty"
-seventy = seven + "ty"
-eighty = eight + "ty"
-ninety = nine + "ty"
-
-
-*/
-  var mydiv = document.getElementById("result");
-  mydiv.innerHTML = outputText;
+const doConvert = favoriteNumber => {
+  // Want to first get the length of the number (# of digits)
+  let numberOfDigits = favoriteNumber.toString().length
+  
+  console.log(numberOfDigits)
+  
+  if (favoriteNumber === 1) return "one"
 }
+
+console.log(doConvert())
