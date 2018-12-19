d9bf784ae55756d123a45d9946af45a8402c69f7
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-19 22:59:25 +0200

    create arrays for our hardcoded data (learning computers talk English...)

--- convertor.js
+++ convertor.js
@@ -7,7 +7,12 @@ const doConvert = favoriteNumber => {
   if (numberOfDigits === 0) return "This is not a valid number"
   if (numberOfDigits > 4) return "A maximum of 4 digits is supported. If you want more digits added get the \"premium version\" :)"
   
-  
+  // create collections of data
+  // I use "zero" as the first string so that the number indexing is more intuitive (for a human!!)
+  const singleDigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nince"]
+  const doubleDigits = ["", "ten", "eleven", "twelve", "thirteen", "fourteen", "sixteen", "seventeen", "eighteen", "nineteen"]
+  const multipleOfTens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+  const tenRaised = ["hundred", "thousand"]
 }
 
 console.log(doConvert())
