2730db0168a63b5d45112278600ca440a611d67d
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-19 17:32:40 +0200

    draft logic for lookup table, concatenations and general mechanism for the convertor

--- convertor.js
+++ convertor.js
@@ -5,8 +5,22 @@ window.doConvert = function () {
   var numberInput = document.getElementById("numberInput").value;
   var outputText = "";
 
+// Create a hardcoded map for n < 20 (all the values when turned to string are uniques)
+/*
+
+Create a hardcoded map for concatenations for n > 20 && n < 100: 
+i.e. 
+tw[o] + en + "ty" 
+th[ree] + ir + "ty"
+fo[ur] + "r" + "ty"
+fi[ve] "f" + "ty" 
+sixty = six + "ty"
+seventy = seven + "ty"
+eighty = eight + "ty"
+ninety = nine + "ty"
 
 
+*/
   var mydiv = document.getElementById("result");
   mydiv.innerHTML = outputText;
 }
