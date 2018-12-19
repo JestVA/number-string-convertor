c6c77d374ba6d9a14b8692b3888c41ccc0bee21c
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2018-12-19 22:32:44 +0200

    define some base cases for UX reasons

--- convertor.js
+++ convertor.js
@@ -1,10 +1,13 @@
 const doConvert = favoriteNumber => {
   // Want to first get the length of the number (# of digits)
   let numberOfDigits = favoriteNumber.toString().length
-  
   console.log(numberOfDigits)
   
-  if (favoriteNumber === 1) return "one"
+  // define some rules for the function for UX
+  if (numberOfDigits === 0) return "This is not a valid number"
+  if (numberOfDigits > 4) return "A maximum of 4 digits is supported. If you want more digits added get the \"premium version\" :)"
+  
+  
 }
 
 console.log(doConvert())
